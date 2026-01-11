ab733fa858ac99591e92ee646448bd68a5ba9129
jay370 <jay560534@gmail.com>
2026-01-11 17:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import registration
+
 import gspread
 from google.oauth2.service_account import Credentials
 import datetime
@@ -45,7 +45,7 @@ st.title("Civil Site Experiment App")
 
 #-- Page Navigation --- 
 if st.button("Contractor Registration"):
-    st.switch_page("registration")
+    st.switch_page("registration.py")
     
 client = get_gspread_client()
 sheet = None
