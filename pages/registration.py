5d228a86e3385de4ccf811a6e934a6e0c466819f
jay370 <jay560534@gmail.com>
2026-01-11 18:32:01 +0530

    update karyu

--- pages/registration.py
+++ pages/registration.py
@@ -33,7 +33,7 @@ with st.form("reg_form", clear_on_submit=True):
                 try:
                     # 'Contractors' tab hovvu joie
                     sheet = client.open("DWCS TWT").worksheet("Contractors")
-                    data = [datetime.datetime.now().strftime("%d-%m-%Y"), con_name, category]
+                    data = [datetime.datetime.now().strftime("%d-%m-%Y"), con_name, category,is_skilled, is_unskilled]
                     sheet.append_row(data)
                     st.success(f"✅ {con_name} Registered!")
                     st.balloons()
