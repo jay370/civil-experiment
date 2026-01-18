27d66ce36700d46d43380bb12df69e272e9335c7
jay370 <jay560534@gmail.com>
2026-01-18 11:51:20 +0530

    title Background Change

--- pages/registration.py
+++ pages/registration.py
@@ -43,7 +43,7 @@ st.markdown("""
             color: #333333;
             font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
             font-weight: 800;
-            background-color: #f0f2f6;
+            background-color: #ffffff;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 1);
