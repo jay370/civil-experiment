358c9a4cf2fd62229596461c0aff1346498e1d6b
jay370 <jay560534@gmail.com>
2026-01-18 11:53:11 +0530

    Update registration.py
    background Color Change

--- pages/registration.py
+++ pages/registration.py
@@ -43,7 +43,7 @@ st.markdown("""
             color: #333333;
             font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
             font-weight: 800;
-            background-color: #ffffff;
+            background-color: #f2f5f9;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 1);
