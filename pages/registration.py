578123ebf6e18b86ee04bc93b8ec87ae39cf310a
jay370 <jay560534@gmail.com>
2026-01-18 12:37:42 +0530

    Update registration.py

--- pages/registration.py
+++ pages/registration.py
@@ -43,7 +43,7 @@ st.markdown("""
             color: #28A745;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             font-size: 25px;
-            font-weight: 800;
+            font-weight: 100;
             background-color: #ffffff;
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
