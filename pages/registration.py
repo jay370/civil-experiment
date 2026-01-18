acb39f7ce9d75d06cf28fc5e78a6316d8f924e29
jay370 <jay560534@gmail.com>
2026-01-18 12:35:40 +0530

    Update registration.py
    Shadow Chages karya che.

--- pages/registration.py
+++ pages/registration.py
@@ -46,7 +46,7 @@ st.markdown("""
             font-weight: 800;
             background-color: #ffffff;
             border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 1);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             border-bottom: 5px solid #007aff;
             ">Contractor Registration</h2>
 """, unsafe_allow_html=True)
