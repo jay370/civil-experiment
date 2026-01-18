6359cd59153beb6e8fcfac99abe2cb39c0fb9a95
jay370 <jay560534@gmail.com>
2026-01-18 12:59:24 +0530

    Update registration.py
    Rectangel box add karyu Text input mate

--- pages/registration.py
+++ pages/registration.py
@@ -23,6 +23,15 @@ st.markdown("""
             .stApp {
                 background-color: #007AFF;
             }
+            /*Rectangle Box (card) ni style */
+            div[data-testid="stVerticalBlockBorderWrapper"] {
+            background-color: #ffffff;
+            border-radius: 15px;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            border: none !important;
+            margin-bottom: 20px;
+            }
             /* Ragister Button-feel  like Apple Button */
             .stButton>button {
                 background-color: #007aff;
@@ -34,8 +43,8 @@ st.markdown("""
                 font-weight: bold;
                 box-shadow: 0px 4px 12px rgba(0, 122, 255, 0, 0.3);
                 border: none;
-
             }
+
 </style> """, unsafe_allow_html=True)
 
 st.markdown("""
@@ -50,3 +59,11 @@ st.markdown("""
             border-bottom: 5px solid #007aff;
             ">Contractor Registration</h2>
 """, unsafe_allow_html=True)
+
+#--- This is Ractangle Box (Card) ---
+with st.container(border=True):
+    st.markdown("<h4 style='color: #000000; font-family: Arial, sans-serif; font-size: 20px; font-weight: 600;'>Personal Details</h4>", unsafe_allow_html=True)
+    #--text input fields--
+    con_name = st.text_input("Contractor Name*",key="con_name", placeholder="Enter Name")
+    con_cat = st.text_input("Contractor Category*",key="con_cat", placeholder="Enter Category")
+    con_phone = st.text_input("Contractor Phone Number*",key="con_phone", placeholder="Enter Phone Number")
