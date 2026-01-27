c19b8ac73230b0e4c22c795b2d4c6b115147d4f7
kaistrom <jay560534@gmail.com>
2026-01-27 23:20:06 +0530

    Update registration.py

--- pages/registration.py
+++ pages/registration.py
@@ -82,9 +82,6 @@ with tab1:
         con_sitename = st.text_input("Contractor Site Name*",key="con_sitename", placeholder="Enter Contractor Site Name")
         con_cat = st.text_input("Contractor Category*",key="con_cat", placeholder="Enter Category")
         con_phone = st.text_input("Contractor Phone Number*",key="con_phone", placeholder="Enter Phone Number")
-        con_email = st.text_input("Contractor Email ID*",key="con_email", placeholder="Enter Email ID")
-        con_gst = st.text_input("Contractor GST Number*",key="con_gst", placeholder="Enter GST Number")
-        con_pan = st.text_input("Contractor PAN Number*",key="con_pan", placeholder="Enter PAN Number")
 
 with tab2:
     st.markdown("### Register Contractor Bank Details")
