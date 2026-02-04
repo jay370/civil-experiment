fe79c0afb40432d5a66575c76e28487545caf3be
kaistrom <jay560534@gmail.com>
2026-02-04 19:55:35 +0530

    Update registration.py

--- pages/registration.py
+++ pages/registration.py
@@ -123,7 +123,7 @@ def save_contractor_smart():
         all_records = sheet.get_all_records()
         
         #2.Duplicate Vender Code Check karvo
-        new_code = st.session_state.get("con_vendercode").strip()
+        new_code = str(st.session_state.get("con_vendercode"))
         
         #3list comprehension thi duplicate code check karvo
         is_Duplicate = any(str(record.get("Vender Code", "")) == str(new_code) for record in all_records)
